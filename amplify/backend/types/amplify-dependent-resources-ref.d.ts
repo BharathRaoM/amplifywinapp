eddb925f2748c2b4252d74599b63048c18a0e946
BharathRaoM <bharath.rao.m@gmail.com>
2021-11-30 02:39:56 -0800

    added storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifywinapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "imagestore": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
